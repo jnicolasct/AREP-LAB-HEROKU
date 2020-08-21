979b9cd17cdb6aa86af845cf4e0b995b1eb2e479
Nicolas Cortes <johan.cortes@mail.escuelaing.edu.co>
2020-08-20 21:37:46 -0500

    correcion final2

--- src/main/java/edu/escuelaing/arep/SparkWebApp.java
+++ src/main/java/edu/escuelaing/arep/SparkWebApp.java
@@ -9,6 +9,7 @@ public class SparkWebApp {
     public static void main(String[] args) {
         port(getPort());
         get("/hello", (req, res) -> "Hello Heroku");
+        get("/", (req, res) -> inputDataPage(req, res));
         get("/inputdata", (req, res) -> inputDataPage(req, res));
         get("/results", (req, res) -> resultsPage(req, res));
 
